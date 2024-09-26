37d09cbaafddbecb0c062014d12815ec0086356e
ErKnot <eliarizzo@hotmail.it>
2024-09-26 15:57:54 +0200

    Starting mdifing MyLinearregressor to create the stochastic gradient descent

--- MyLinearregressor.py
+++ MyLinearregressor.py
@@ -10,12 +10,39 @@ class MyLinearRegressor(Predictor):
         self._num_rows = x.shape[0]
         self._vect_of_ones = np.ones((self._num_rows,1))
         self._X = np.concatenate((self._vect_of_ones, x), axis=1)
+        self._y = y
         self._coefficients = MyMSE().fit(x, y).predict()
+
+        self._Xy = np.c_[self._X, self._y]
+
         return self
         
-    def predict(self, x: np.ndarray) -> np.array:
-        self._X = np.concatenate((self._vect_of_ones, x), axis=1)
-        return np.dot(self._X, self._coefficients)
+    def predict(cost_function, stat=None, learn_rate=0.1, decay_rate=0.0, batch_size=1; n_iter=100, tolerance=1e-06, dtype: str = "float64", random_state=None):
+
+        # Checing if the cost funcion is callable
+        if not callable(cost_function):
+            raise TypeError("'cost_function' must be callable")
+
+        seed = None if random_state is None else int(random_state)
+        rng = np.random.default_rng(seed=seed)
+
+        for _ in range(n_iter):
+            # shuffle x and y
+            rng.shuffle(self._Xy)
+
+            # Performing minibatch moves
+            for start in range(0, self._num_rows, batch_size):
+                stop = batch_size + start
+                X_batch, y_barch = self._Xy[start:stop, :-1], self._Xy[start:stop, -1:]
+
+                # Calculating the gradiente
+                mse = MyMSE().fit(X_batch, y_batch).gradient
+        
+
+
+    # def predict(self, x: np.ndarray) -> np.array:
+    #     self._X = np.concatenate((self._vect_of_ones, x), axis=1)
+    #     return np.dot(self._X, self._coefficients)
     
     def get_coefficients(self):
         return self._coefficients
